27534cdaf3859aae2a263d5f4de5683eb49c8fdd
bradynp <bpbraithwaite@gmail.com>
2021-04-13 19:25:37 -0700

    outline

--- src/main.ino
+++ src/main.ino
@@ -2,7 +2,16 @@
 Bradyn Braithwaite 2021
 */
 
-#include "index.h"
+#include "credentials.h"
+#include "index.hpp"
 
-void setup() {}
-void loop() {}
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+
+ESP8266WebServer *server;
+
+void handleRoot() {}
+void handleNotFound() {}
+
+void setup() { server = new ESP8266WebServer(80); }
+void loop() { server.handleClient(); }
