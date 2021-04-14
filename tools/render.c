b36b1ca050d2f5ab738edac9108e80c1cc08a515
bradynp <bpbraithwaite@gmail.com>
2021-04-13 19:25:52 -0700

    all functionality implemented

--- tools/render.c
+++ tools/render.c
@@ -1,3 +1,29 @@
 // copy index.html into formatted file index.h
+// not very cohesive but it gets the job done
+#include <stdio.h>
+#include <string.h>
 
-int main() { return 0; }
+int main() {
+
+  const char *preamble =
+      "#pragma once\n#include \"pgmspace.h\"\nconst char ndx[] = R\"=====(\n";
+  const char *postamble = "\n)=====\";";
+
+  FILE *infile = fopen("..\\src\\index.html", "r");
+  FILE *outfile = fopen("..\\src\\index.hpp", "w");
+
+  fprintf(outfile, preamble);
+  long lines = 1;
+  char c;
+  while ((c = (char)fgetc(infile)) != EOF) {
+    fprintf(outfile, "%c", c);
+    lines += (c == '\n');
+  }
+
+  fprintf(outfile, postamble);
+  fclose(outfile);
+  fclose(infile);
+  printf("\nDone copying %ld lines.", lines);
+
+  return 0;
+}
