46674160ff0db6165d1118675a046259157a059c
bradynp <bpbraithwaite@gmail.com>
2021-04-13 19:25:31 -0700

    tested copying ndx

--- src/index.hpp
+++ src/index.hpp
@@ -1,5 +1,22 @@
 #pragma once
 #include "pgmspace.h"
-
 const char ndx[] = R"=====(
-    )=====";
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+</head>
+<style>
+
+</style>
+
+<body>
+    <script>
+
+    </script>
+</body>
+
+</html>
+
+)=====";
