d13bfa4581efec0dd5ae790b6956f0e093c65fee
bradynp <bpbraithwaite@gmail.com>
2021-04-16 22:16:48 -0700

    skeleton

--- src/main.ino
+++ src/main.ino
@@ -2,16 +2,46 @@
 Bradyn Braithwaite 2021
 */
 
-#include "credentials.h"
-#include "index.hpp"
+#include "credentials.h" // contains ssid and pwsd
+#include "index.hpp"     // contains ndx
 
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 
+#define nodegpio_d2 4
+
 ESP8266WebServer *server;
+static uint8_t relayState = 0;
 
-void handleRoot() {}
-void handleNotFound() {}
+void handleRoot() { server->send(200, "text/html", String(ndx)); }
+void handleNotFound() { server->send(404); }
+void handleS0() {}
+void handleS1() {}
+void handleGETS() {
 
-void setup() { server = new ESP8266WebServer(80); }
-void loop() { server.handleClient(); }
+  relayState = digitalRead(nodegpio_d2);
+  server->send(200, "text/plain", String(relayState));
+}
+
+void setup() {
+
+  server = new ESP8266WebServer(80);
+
+  pinMode(nodegpio_d2, OUTPUT);
+
+  delay(100);
+  WiFi.begin(ssid, pswd);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+  }
+
+  server->onNotFound(handleNotFound);
+  server->on("/", handleRoot);
+  server->on("/off", handleS0);
+  server->on("/on", handleS1);
+  server->begin();
+}
+void loop() {
+  server->handleClient();
+  yield();
+}
