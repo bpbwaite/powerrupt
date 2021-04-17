29a0f72b147ac654942c0eefa3203c200e3fe582
bradynp <bpbraithwaite@gmail.com>
2021-04-16 23:00:29 -0700

    server-side security implementation

--- src/main.ino
+++ src/main.ino
@@ -5,43 +5,56 @@ Bradyn Braithwaite 2021
 #include "credentials.h" // contains ssid and pwsd
 #include "index.hpp"     // contains ndx
 
+#include <ESP8266HTTPClient.h> // only used for definitions
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 
 #define nodegpio_d2 4
+#define longpressms 470
 
 ESP8266WebServer *server;
-static uint8_t relayState = 0;
+static unsigned long toflastclickr = 0;
 
-void handleRoot() { server->send(200, "text/html", String(ndx)); }
-void handleNotFound() { server->send(404); }
-void handleS0() {}
-void handleS1() {}
-void handleGETS() {
+void handleRoot() {
+    server->send(HTTP_CODE_OK, "text/html", String(ndx));
+}
+void handleNotFound() {
+    server->send(HTTP_CODE_NOT_FOUND);
+}
 
-  relayState = digitalRead(nodegpio_d2);
-  server->send(200, "text/plain", String(relayState));
+void handleS1() {
+    if (toflastclickr + 5000L > millis()) {
+        toflastclickr = millis();
+
+        server->send(HTTP_CODE_OK);
+
+        digitalWrite(nodegpio_d2, HIGH);
+        delay(longpressms);
+        digitalWrite(nodegpio_d2, LOW);
+    }
+    else {
+        server->send(HTTP_CODE_FORBIDDEN, "text/html", String("pls no hack me :("));
+    }
 }
 
 void setup() {
 
-  server = new ESP8266WebServer(80);
+    server = new ESP8266WebServer(80);
 
-  pinMode(nodegpio_d2, OUTPUT);
+    digitalWrite(nodegpio_d2, LOW);
+    pinMode(nodegpio_d2, OUTPUT);
 
-  delay(100);
-  WiFi.begin(ssid, pswd);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(250);
-  }
+    WiFi.begin(ssid, pswd);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(250);
+    }
 
-  server->onNotFound(handleNotFound);
-  server->on("/", handleRoot);
-  server->on("/off", handleS0);
-  server->on("/on", handleS1);
-  server->begin();
+    server->onNotFound(handleNotFound);
+    server->on("/", handleRoot);
+    server->on("/rclick", handleS1);
+    server->begin();
 }
 void loop() {
-  server->handleClient();
-  yield();
+    server->handleClient();
+    yield();
 }
